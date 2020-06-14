ab39df408b2d4b27151f000f4732b0405b201323
Marques Silva Fonseca <marques-fonseca@hotmail.com>
2020-06-13 23:01:15 -0300

    Projeto MVC

---
+++ Ola-Mundo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ola_Mundo.Startup))]
+namespace Ola_Mundo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
